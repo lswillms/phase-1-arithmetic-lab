f778a13d33c9fa439fbbca7b79a3450bb38b1a9c
Latoshia Williams <lswillms@yahoo.com>
2022-07-02 14:13:57 -0500

    commit tree 3563aa322d4414d9fe8822e34a1636f53ec1d8fa

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+const num1 = 2
+const num2 = 31
+let multiply = num1*num2
+
+let random = Math.round(Math.random())+1
+
+const num3 = 28
+const num4 = 6
+let mod = num3 % num4
+
+let max = Math.max(5,10,15,20)
